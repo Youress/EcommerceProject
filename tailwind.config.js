dee8e9d3eead19b4340709f2bc0c3723bdff3033
Youssef Essouairi <rajawiiyoussef@gmail.com>
2024-04-26 16:16:50 +0100

    imageSlider

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,25 +6,6 @@ module.exports = {
       gridTemplateColumns: {
         "twocol": "55fr 45fr",
       },
-      screens: {
-        '2xl': {'max': '1535px'},
-        // => @media (max-width: 1535px) { ... }
-  
-        'xl': {'max': '1279px'},
-        // => @media (max-width: 1279px) { ... }
-  
-        'lg': {'max': '999px'},
-        // => @media (max-width: 1023px) { ... }
-
-        'xmd': {'max': '900px'},
-        // => @media (max-width: 900px) { ... }
-
-        'md': {'max': '767px'},
-        // => @media (max-width: 767px) { ... }
-  
-        'sm': {'max': '600px'},
-        // => @media (max-width: 426px) { ... }
-      },
       spacing: {
         '38-percent-minus-44': 'calc(38% - 44px)',
       },
